5ad4cc56827f4ad98f175b8070b5af4addf837f8
Vitor Sousa <vitorsousavhcs@outlook.com>
2024-04-21 07:53:02 +0100

    Add readme and minor improvements

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# rag-llama3-elasticsearch
-RAG with LlamaIndex, Elasticsearch and Llama3 Hands-On
+# RAG with LlamaIndex, Elasticsearch and Llama3 🦙
+
+This repository houses a powerful tool that blends **Streamlit**, **Elasticsearch**, and cutting-edge language models like **Llama3**. It's designed to simplify querying documents via conversation vectors embedded into a dynamic, user-friendly web interface.
+
+## Features 🌟
+
+- **Document Upload:** Upload JSON files with ease.
+- **Efficient Search:** Powered by Elasticsearch.
+- **Natural Language Queries:** Harness models like Llama3 to query using everyday language.
+- **Interactive UI:** Real-time updates and feedback.
+- **Scalable:** Built to manage extensive data volumes and complex queries.
+
+## Getting Started 🚀
+
+### Prerequisites
+
+- Python 3.8+
+- Elasticsearch 7.x+
+- Ollama
+- llama_index
+- Streamlit
+
+### Installation
+
+Clone and set up the project:
+
+```bash
+git clone https://github.com/your-username/document-query-interface.git
+cd document-query-interface
+pip install -r requirements.txt
+```
+
+### Usage
+Launch the app:
+
+```bash
+poetry run streamlit run app.py
+```
+
+### Structure 📂
+app.py: The Streamlit application entry point.
+index/: Handles document indexing in Elasticsearch.
+query/: Manages the query engine setup and execution.
+assets/: Static files like images for the UI.
+
+### Contributing 🤝
+Interested in contributing? Great! Here's how:
+
+1. Fork the repo.
+2. Create your feature branch (git checkout -b feature-branch).
+3. Commit your changes (git commit -am 'Add some feature').
+4. Push to the branch (git push origin feature-branch).
+5. Create a new Pull Request.
--- app.py
+++ app.py
@@ -5,31 +5,34 @@ from elasticsearch import AsyncElasticsearch
 from index import get_documents_from_file, ingest_documents
 from query import setup_query_engine, execute_query
 import time
+
 # Streamlit page configuration
 st.set_page_config(page_title="Document Query Interface", layout="wide")
 
+es_client = AsyncElasticsearch("http://localhost:9200")
+
 async def check_embeddings():
-    es_client = AsyncElasticsearch("http://localhost:9200")
-    index_info = await es_client.indices.exists(index="calls")
-    if index_info:
-        doc_count = await es_client.count(index="calls", body={"query": {"exists": {"field": "conversation_vector"}}})
-        embeddings_present = doc_count['count'] > 0
-    else:
-        embeddings_present = False
-    return embeddings_present
+    try:
+        index_info = await es_client.indices.exists(index="calls")
+        if index_info:
+            doc_count = await es_client.count(index="calls", body={"query": {"exists": {"field": "conversation_vector"}}})
+            return doc_count['count'] > 0
+        return False
+    except Exception as e:
+        st.error(f"Failed to connect or query Elasticsearch: {e}")
+        return False
 
 def main():
     banner = Image.open('assets/img_1.png')
     st.markdown("<h1 style='text-align: center; color: #01bfaa;'>RAG with Elasticsearch and Llama3 Hands-On</h1>", unsafe_allow_html=True)
 
     st.sidebar.image(banner)
-    st.markdown("<h2 style='text-align: center; color: #e16217;'>Select the Model</h2>", unsafe_allow_html=True)
-    model_name = st.selectbox("", ["llama3", "llama2", "mistral"], index=0)
-    st.markdown("<h2 style='text-align: center; color: #fb9b43;'>Load the Documents</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #e16217;'>Select the Model 🦙</h2>", unsafe_allow_html=True)
+    model_name = st.selectbox("", ["llama3", "llama2"], index=0)
+    st.markdown("<h2 style='text-align: center; color: #fb9b43;'>Load the Documents 📁</h2>", unsafe_allow_html=True)
 
     embeddings_present = asyncio.run(check_embeddings())
 
-    json_file = None
     if not embeddings_present:
         json_file = st.file_uploader("Upload a JSON file with conversations", type=['json'])
         if json_file is not None:
@@ -38,29 +41,26 @@ def main():
     else:
         st.success("Embeddings are already present in Elasticsearch.")
 
-    st.markdown("<h2 style='text-align: center; color: #ffe79c;'>Enter your query</h2>", unsafe_allow_html=True)
-
+    st.markdown("<h2 style='text-align: center; color: #ffe79c;'>Enter your query ⌨️</h2>", unsafe_allow_html=True)
     query_text = st.text_input("", "Give me a summary of water related issues")
 
     if st.button('Run Query'):
-        query_engine = setup_query_engine(model_name)
-        start_time = time.time()
-        result = execute_query(query_engine, query_text)
+        try:
+            query_engine = setup_query_engine(model_name)
+            start_time = time.time()
+            result = execute_query(query_engine, query_text)
+            end_time = time.time()
+            execution_time = round(end_time - start_time, 2)
+            st.success(result.response if result else "No results found.")
+            st.info(f"Query executed in {execution_time} seconds")
+        except Exception as e:
+            st.error(f"Failed to execute query: {e}")
 
-        end_time = time.time()
-
-        execution_time = round(end_time - start_time, 2)
-        st.write(result.response if result else "No results found.")
-        st.write(f"Query executed in {execution_time} seconds")
     st.markdown("""
-    <br> <br>
-    <br> <br>
-    <br> <br>
-    <br> <br>
-    <br>
         <footer style='text-align: center; color: grey; padding: 10px; background-color: #f5f5f5;'>
             © 2024 Vitor Sousa.
         </footer>
         """, unsafe_allow_html=True)
+
 if __name__ == "__main__":
     main()
